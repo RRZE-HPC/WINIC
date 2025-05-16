49e817c76e58408739bc9f6884714af0e5ecdb6e
Tobias Ruehl <ts.rl@web.de>
2025-05-16 13:22:19 +0200

    Fix setup

--- setup.sh
+++ setup.sh
@@ -3,52 +3,51 @@
 set -e
 
 # Clone llvm
-git clone --branch=19.x --depth=1 https://github.com/llvm/llvm-project.git
-cd llvm-project
-git sparse-checkout init --cone
-git sparse-checkout set llvm clang third-party cmake
+if [ -d "llvm-project" ]; then
+    echo "LLVM repo already cloned." 
+else
+    git clone -b release/20.x --depth=1 https://github.com/llvm/llvm-project.git
+    cd llvm-project
+    git sparse-checkout init --cone
+    git sparse-checkout set llvm clang third-party cmake
+fi
 
-DIR_ARG="build-llvm"
+BUILD_DIR="build"
 
 # Parse arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        --dir) DIR_ARG="$2"; shift ;;
+        --dir) BUILD_DIR="$2"; shift ;;
         --help) echo "Usage: $0 [--dir buildDirName]"; exit 0 ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
-#make path relative
-DIR_ARG="./$DIR_ARG"
-
+LLVM_BUILD_DIR="./lb-$BUILD_DIR"
 
 # Check if build dir exists
-if [ -d "$DIR_ARG" ]; then
-    echo "Build dir '$DIR_ARG' already exists."
-    cd ./$DIR_ARG
+if [ -d "$LLVM_BUILD_DIR" ]; then
+    echo "Using existing build dir '$LLVM_BUILD_DIR'."
+    cd $LLVM_BUILD_DIR
 else
-    echo "Directory '$DIR_ARG' does not exist, building LLVM and Clang there."
-    mkdir -p ./$DIR_ARG && cd ./$DIR_ARG
+    echo "Directory '$LLVM_BUILD_DIR' does not exist, building LLVM and Clang there."
+    mkdir -p $LLVM_BUILD_DIR && cd $LLVM_BUILD_DIR
+    # Build llvm and clang
+    cmake -S ../llvm-project/llvm -B . \
+    -DLLVM_ENABLE_PROJECTS=clang \
+    -DLLVM_TARGETS_TO_BUILD="X86;AArch64;RISCV" \
+    -DCMAKE_BUILD_TYPE=Release
+
+    cmake --build . -- -j 90
 fi
 
-
-# Build llvm and clang
-cmake -S ./llvm-project/llvm -B . \
-  -DLLVM_ENABLE_PROJECTS=clang \
-  -DLLVM_TARGETS_TO_BUILD="X86;AArch64;RISCV" \
-  -DCMAKE_BUILD_TYPE=Release
-
-cmake --build . -- -j 90
-
-
-# mkdir -p ../build && cd ../build
-cmake -DCMAKE_BUILD_TYPE=Release \
+mkdir -p ../$BUILD_DIR && cd ../$BUILD_DIR
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_SOURCE_DIR=../llvm-project/llvm \
-  -DLLVM_BINARY_DIR=../llvm-build \
-  -DLLVM_DIR=../$DIR_ARG/lib/cmake/llvm \
-  -DClang_DIR=../$DIR_ARG/lib/cmake/clang \
-  -DCLANG_PATH=../$DIR_ARG/bin/clang ..
+  -DLLVM_BINARY_DIR=../$LLVM_BUILD_DIR \
+  -DLLVM_DIR=../$LLVM_BUILD_DIR/lib/cmake/llvm \
+  -DClang_DIR=../$LLVM_BUILD_DIR/lib/cmake/clang \
+  -DCLANG_PATH=../$LLVM_BUILD_DIR/bin/clang ..
   
 cmake --build . -- -j 6
