2b79877bd699156a2ad5b112d338b70244511124
Tobias Ruehl <ts.rl@web.de>
2026-07-04 12:23:59 +0200

    add printing style for MCInstDesc and MCOperandInfo

--- include/LLVMDebug.h
+++ include/LLVMDebug.h
@@ -8,6 +8,8 @@
 #include "llvm/MC/MCInstrInfo.h"
 #include "llvm/MC/MCRegister.h"
 #include "llvm/MC/MCRegisterInfo.h"
+#include <cstdint>
+#include <llvm/MC/MCInstrDesc.h>
 #include <memory>
 #include <ostream>
 
@@ -42,6 +44,51 @@ inline std::ostream &operator<<(std::ostream &OS, const MCInst &Inst) {
         OS << ", op" << i << ": " << Inst.getOperand(i);
     }
     return OS << ">";
+}
+
+constexpr std::string_view opToString(uint8_t OperandType) {
+    switch (OperandType) {
+    case MCOI::OPERAND_UNKNOWN:
+        return "OPERAND_UNKNOWN";
+    case MCOI::OPERAND_IMMEDIATE:
+        return "OPERAND_IMMEDIATE";
+    case MCOI::OPERAND_REGISTER:
+        return "OPERAND_REGISTER";
+    case MCOI::OPERAND_MEMORY:
+        return "OPERAND_MEMORY";
+    case MCOI::OPERAND_PCREL:
+        return "OPERAND_PCREL";
+    case MCOI::OPERAND_GENERIC_0:
+        return "OPERAND_GENERIC_0";
+    case MCOI::OPERAND_GENERIC_1:
+        return "OPERAND_GENERIC_1";
+    case MCOI::OPERAND_GENERIC_2:
+        return "OPERAND_GENERIC_2";
+    case MCOI::OPERAND_GENERIC_3:
+        return "OPERAND_GENERIC_3";
+    case MCOI::OPERAND_GENERIC_4:
+        return "OPERAND_GENERIC_4";
+    case MCOI::OPERAND_GENERIC_5:
+        return "OPERAND_GENERIC_5";
+    case MCOI::OPERAND_GENERIC_IMM_0:
+        return "OPERAND_GENERIC_IMM_0";
+    case MCOI::OPERAND_FIRST_TARGET:
+        return "OPERAND_FIRST_TARGET";
+    default:
+        return "unknown";
+    }
+}
+
+inline std::ostream &operator<<(std::ostream &OS, const MCOperandInfo &OpInfo) {
+    return OS << opToString(OpInfo.OperandType);
+}
+
+inline std::ostream &operator<<(std::ostream &OS, const MCInstrDesc &InstrDesc) {
+    OS << "MCInstrDesc<" << ::winic::getEnv().MCII->getName(InstrDesc.getOpcode());
+    for (unsigned i = 0; i < InstrDesc.getNumOperands(); ++i) {
+        OS << ", op" << i << ": " << InstrDesc.operands()[i];
+    }
+    return OS << " numDefs: " << InstrDesc.getNumDefs() << ">";
 }
 
 } // namespace llvm
