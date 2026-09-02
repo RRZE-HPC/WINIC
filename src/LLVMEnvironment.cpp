RRZE-HPC/WINIC

commit 3a3729e4f87f56b420502e1cdf6f01be8aa4a7fd

encapsulate all architecture checks in helper functions

src/LLVMEnvironment.cpp
@@ -121,14 +121,25 @@ ErrorCode LLVMEnvironment::setUp(std::string March, std::string Cpu) {
     //                                        *>(Machine.get()), stimpl, functionNum, *MMI.get());
     TRI = MF->getSubtarget().getRegisterInfo();
     MaxReg = TRI->getNumSupportedRegs(*MF);
-    Arch = MSTI->getTargetTriple().getArch(); // for convenience
     return SUCCESS;
+}
+
+bool LLVMEnvironment::isX86() {
+    return MSTI->getTargetTriple().getArch() == Triple::ArchType::x86_64;
+}
+
+bool LLVMEnvironment::isAArch64() {
+    return MSTI->getTargetTriple().getArch() == Triple::ArchType::aarch64;
+}
+
+bool LLVMEnvironment::isRISCV() {
+    return MSTI->getTargetTriple().getArch() == Triple::ArchType::riscv64;
 }
 
 unsigned LLVMEnvironment::getMemoryOperandWidthUpperBound(unsigned Opcode) {
     const MCInstrDesc &desc = getEnv().MCII->get(Opcode);
 
-    if (getEnv().Arch == llvm::Triple::aarch64) {
+    if (getEnv().isAArch64()) {
         // check if the target specific helpers can provide an exact width
         TypeSize scale(0U, false), width(0U, false);
         int64_t minOffset, maxOffset;
@@ -261,7 +272,7 @@ unsigned LLVMEnvironment::getTiedToOperand(MCOperandInfo OpInfo) {
 }
 
 unsigned LLVMEnvironment::getAArch64OffsetOperandIndex(unsigned Opcode) {
-    if (getEnv().Arch != llvm::Triple::aarch64) return NO_OP_INDEX;
+    if (getEnv().isAArch64()) return NO_OP_INDEX;
     // verify this is a memory instruction as llvm does in AArch64InstrInfo::verifyInstruction
     TypeSize scale(0U, false), width(0U, false);
     int64_t minOffset, maxOffset;
@@ -274,7 +285,7 @@ unsigned LLVMEnvironment::getAArch64OffsetOperandIndex(unsigned Opcode) {
 }
 
 unsigned LLVMEnvironment::getAArch64BaseOperandIndex(unsigned Opcode) {
-    if (getEnv().Arch != llvm::Triple::aarch64) return NO_OP_INDEX;
+    if (getEnv().isAArch64()) return NO_OP_INDEX;
     TypeSize scale(0U, false), width(0U, false);
     int64_t minOffset, maxOffset;
     if (!AArch64InstrInfo::getMemOpInfo(Opcode, scale, width, minOffset, maxOffset))

include/AssemblyFile.h
@@ -2,7 +2,6 @@
 #define ASSEMBLY_FILE_H
 
 #include "ErrorCode.h"
-#include "llvm/TargetParser/Triple.h"
 #include <set>
 #include <string>
 
@@ -48,11 +47,7 @@ class AssemblyFile {
   public:
     AssemblyFile() = default;
 
-    AssemblyFile(llvm::Triple::ArchType Arch) { this->arch = Arch; }
-
     ~AssemblyFile() = default;
-
-    void setArch(llvm::Triple::ArchType Arch) { this->arch = Arch; }
 
     /**
      * \brief Adds an initialization function to the assembly file.
@@ -107,7 +102,6 @@ class AssemblyFile {
     std::string generateAssembly();
 
   private:
-    llvm::Triple::ArchType arch;
     std::string name;
     std::set<BenchFunction> benchFunctions;
     std::set<InitFunction> initFunctions;

include/Globals.h
@@ -258,7 +258,7 @@ class OperandForm {
     void setMemoryOperand(MCInst *Inst, MCRegister BaseRegister, unsigned Displacement) {
         assert(isMemory());
         initMCInst(Inst);
-        if (getEnv().Arch == llvm::Triple::x86_64) {
+        if (getEnv().isX86()) {
             X86MemoryOperand *memOp = std::get_if<X86MemoryOperand>(&kind);
 
             for (unsigned index : memOp->baseIndices)
@@ -271,14 +271,14 @@ class OperandForm {
                 Inst->getOperand(index) = MCOperand::createImm(Displacement);
             for (unsigned index : memOp->segmentIndices)
                 Inst->getOperand(index) = MCOperand::createReg(0);
-        } else if (getEnv().Arch == llvm::Triple::aarch64) {
+        } else if (getEnv().isAArch64()) {
             AArch64MemoryOperand *memOp = std::get_if<AArch64MemoryOperand>(&kind);
 
             for (unsigned index : memOp->baseIndices)
                 Inst->getOperand(index) = MCOperand::createReg(BaseRegister);
             for (unsigned index : memOp->offsetIndices)
                 Inst->getOperand(index) = MCOperand::createImm(Displacement);
-        } else if (getEnv().Arch == llvm::Triple::riscv64) {
+        } else if (getEnv().isRISCV()) {
             out(std::cerr, "RISCV memory not implemented yet");
         }
     }
@@ -288,16 +288,16 @@ class OperandForm {
      */
     unsigned getMemoryOperandOffset(MCInst Inst) {
         assert(isMemory());
-        if (getEnv().Arch == llvm::Triple::x86_64) {
+        if (getEnv().isX86()) {
             X86MemoryOperand *memOp = std::get_if<X86MemoryOperand>(&kind);
             return Inst.getOperand(memOp->offsetIndices[0]).getImm();
         }
-        if (getEnv().Arch == llvm::Triple::aarch64) {
+        if (getEnv().isAArch64()) {
             AArch64MemoryOperand *memOp = std::get_if<AArch64MemoryOperand>(&kind);
             // dbg(__func__, memOp->offsetIndices);
             return Inst.getOperand(memOp->offsetIndices[0]).getImm();
         }
-        if (getEnv().Arch == llvm::Triple::riscv64) {
+        if (getEnv().isRISCV()) {
             out(std::cerr, "RISCV memory not implemented yet");
         }
         return NO_OP_INDEX;

include/LLVMEnvironment.h
@@ -45,7 +45,6 @@ class LLVMEnvironment {
     std::unique_ptr<MCInstPrinter> MIP;
 
     unsigned MaxReg;
-    Triple::ArchType Arch;
 
     /**
      * \brief Constructs a new LLVMEnvironment and initializes the LLVM context and module.
@@ -62,6 +61,21 @@ class LLVMEnvironment {
      * \return ErrorCode indicating success or failure.
      */
     ErrorCode setUp(std::string March = "", std::string Cpu = "");
+
+    /**
+     * \brief Checks the current architecture is x86
+     */
+    bool isX86();
+
+    /**
+     * \brief Checks the current architecture is AArch64
+     */
+    bool isAArch64();
+
+    /**
+     * \brief Checks the current architecture is RISCV
+     */
+    bool isRISCV();
 
     unsigned getMemoryOperandWidthUpperBound(unsigned Opcode);
 

include/Templates.h
@@ -47,7 +47,7 @@ extern Template X86Template;
 extern Template AArch64Template;
 extern Template RISCVTemplate;
 
-Template getTemplate(llvm::Triple::ArchType Arch);
+Template getTemplate();
 
 } // namespace winic
 

src/AssemblyFile.cpp
@@ -101,13 +101,9 @@ unsigned AssemblyFile::getNumInstFor(std::string BenchName) {
  * @return std::string Assembly code as a string.
  */
 std::string AssemblyFile::generateAssembly() {
-    if (arch == 0) {
-        std::cerr << "called generateAssembly on uninitialized AssemblyFile" << std::endl;
-        return "";
-    }
     std::string result;
     llvm::raw_string_ostream rso(result);
-    Template benchTemplate = getTemplate(arch);
+    Template benchTemplate = getTemplate();
     rso << benchTemplate.prefix;
     for (BenchFunction function : benchFunctions)
         rso << generateBenchFunction(function) << "\n";
@@ -120,7 +116,7 @@ std::string AssemblyFile::generateAssembly() {
 std::string AssemblyFile::generateBenchFunction(BenchFunction Function) {
     std::string result;
     llvm::raw_string_ostream rso(result);
-    Template benchTemplate = getTemplate(arch);
+    Template benchTemplate = getTemplate();
     rso << replaceFunctionName(benchTemplate.preLoop, Function.name);
     rso << indentBlock(Function.preLoopCode, 2);
     rso << replaceFunctionName(benchTemplate.beginLoop, Function.name);
@@ -134,7 +130,7 @@ std::string AssemblyFile::generateBenchFunction(BenchFunction Function) {
 std::string AssemblyFile::generateInitFunction(InitFunction Function) {
     std::string result;
     llvm::raw_string_ostream rso(result);
-    Template benchTemplate = getTemplate(arch);
+    Template benchTemplate = getTemplate();
     rso << replaceFunctionName(benchTemplate.preInit, Function.name);
     rso << indentBlock(Function.initCode, 2);
     rso << replaceFunctionName(benchTemplate.postInit, Function.name);

src/BenchmarkGenerator.cpp
@@ -21,7 +21,6 @@
 #include "llvm/MC/MCRegisterInfo.h"
 #include "llvm/MC/MCSubtargetInfo.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/TargetParser/Triple.h"
 #include <algorithm>
 #include <cstddef>
 #include <initializer_list>
@@ -64,7 +63,7 @@ genLatBenchmark(const std::vector<LatMeasurement> &Measurements, unsigned *Targe
                 std::set<MCRegister> UsedRegisters, long RegInitValue, long Immediate) {
     dbg(__func__, "Measurements.size(): ", Measurements.size(),
         " TargetInstrCount: ", *TargetInstrCount, " UsedRegisters.size(): ", UsedRegisters.size());
-    auto benchTemplate = getTemplate(getEnv().MSTI->getTargetTriple().getArch());
+    auto benchTemplate = getTemplate();
     // extract list of registers used by the template
     for (unsigned i = 0; i < getEnv().MRI->getNumRegs(); i++) {
         MCRegister reg = MCRegister::from(i);
@@ -161,7 +160,7 @@ genLatBenchmark(const std::vector<LatMeasurement> &Measurements, unsigned *Targe
     }
     ico << restoreRegs << "\n";
 
-    AssemblyFile assemblyFile(getEnv().Arch);
+    AssemblyFile assemblyFile;
     assemblyFile.addInitFunction("init", initCode);
     assemblyFile.addBenchFunction("lat", saveRegs + regInit, loopCode, restoreRegs, "init",
                                   *TargetInstrCount * instructions.size());
@@ -190,7 +189,7 @@ genTPBenchmark(unsigned Opcode, unsigned *TargetInstrCount, unsigned UnrollCount
         " HelperConstraints.size(): ", HelperConstraints.size());
     if (HelperOpcode != MAX_UNSIGNED)
         dbg(__func__, "Helper: ", getEnv().MCII->getName(HelperOpcode));
-    auto benchTemplate = getTemplate(getEnv().MSTI->getTargetTriple().getArch());
+    auto benchTemplate = getTemplate();
     // extract list of registers used by the template
     // TODO optimize
     for (unsigned i = 0; i < getEnv().MRI->getNumRegs(); i++) {
@@ -250,7 +249,7 @@ genTPBenchmark(unsigned Opcode, unsigned *TargetInstrCount, unsigned UnrollCount
 
     std::string initCode = saveRegs + singleLoopCode + restoreRegs + "\n";
 
-    AssemblyFile assemblyFile(getEnv().Arch);
+    AssemblyFile assemblyFile;
     assemblyFile.addInitFunction("init", initCode);
     assemblyFile.addBenchFunction("tp", saveRegs + regInit, loopCode, restoreRegs, "init",
                                   instructions.size());
@@ -344,8 +343,7 @@ genInst(unsigned Opcode, std::map<unsigned, MCRegister> Constraints,
             continue;
         }
         if (op.isMemory()) {
-            op.setMemoryOperand(&inst, getTemplate(getEnv().Arch).scratchMemoryBaseReg,
-                                MemDisplacement);
+            op.setMemoryOperand(&inst, getTemplate().scratchMemoryBaseReg, MemDisplacement);
         }
         if (op.isImmediate()) {
             op.setImmediateOperand(&inst, Immediate);
@@ -354,8 +352,7 @@ genInst(unsigned Opcode, std::map<unsigned, MCRegister> Constraints,
             bool foundRegister = false;
             const MCRegisterClass &regClass = getEnv().MRI->getRegClass(op.getRegClassID());
             for (MCRegister reg : regClass) {
-                if ((getEnv().Arch == Triple::ArchType::x86_64 && reg.id() == X86::RIP) ||
-                    reg.id() >= getEnv().MaxReg)
+                if ((getEnv().isX86() && reg.id() == X86::RIP) || reg.id() >= getEnv().MaxReg)
                     // RIP register (58) is included in GR64 class which is a bug as of
                     // LLVM 22.1.8 see X86RegisterInfo.td:586
                     continue;
@@ -454,25 +451,18 @@ std::pair<ErrorCode, std::string> genSaveRegister(MCRegister Reg) {
     std::string result;
     llvm::raw_string_ostream os(result); // Wrap with raw_ostream
 
-    switch (getEnv().Arch) {
-    case llvm::Triple::x86_64: {
+    if (getEnv().isX86()) {
         MCInst inst;
         inst.setOpcode(getEnv().getOpcode("PUSH64r"));
         inst.clear();
         inst.addOperand(MCOperand::createReg(Reg));
         getEnv().MIP->printInst(&inst, 0, "", *getEnv().MSTI, os);
         os << "\n";
-        break;
+        return {SUCCESS, result};
     }
-    case llvm::Triple::aarch64:
-        return {SUCCESS, ""}; // all registers saved in template
-    case llvm::Triple::riscv64:
-        return {SUCCESS, ""}; // all registers saved in template
-    default:
-        return {E_UNSUPPORTED_ARCH, ""};
-    }
-
-    return {SUCCESS, result};
+    if (getEnv().isAArch64()) return {SUCCESS, ""}; // all registers saved in template
+    if (getEnv().isRISCV()) return {SUCCESS, ""};   // all registers saved in template
+    return {E_UNSUPPORTED_ARCH, ""};
 }
 
 std::pair<ErrorCode, std::string> genRestoreRegister(MCRegister Reg) {
@@ -482,24 +472,18 @@ std::pair<ErrorCode, std::string> genRestoreRegister(MCRegister Reg) {
     std::string result;
     llvm::raw_string_ostream os(result);
 
-    switch (getEnv().Arch) {
-    case llvm::Triple::x86_64: {
+    if (getEnv().isX86()) {
         MCInst inst;
         inst.setOpcode(getEnv().getOpcode("POP64r"));
         inst.clear();
         inst.addOperand(MCOperand::createReg(Reg));
         getEnv().MIP->printInst(&inst, 0, "", *getEnv().MSTI, os);
         os << "\n";
-        break;
+        return {SUCCESS, result};
     }
-    case llvm::Triple::aarch64:
-        return {SUCCESS, ""}; // all registers restored in template
-    case llvm::Triple::riscv64:
-        return {SUCCESS, ""}; // all registers restored in template
-    default:
-        return {E_UNSUPPORTED_ARCH, ""};
-    }
-    return {SUCCESS, result};
+    if (getEnv().isAArch64()) return {SUCCESS, ""}; // all registers restored in template
+    if (getEnv().isRISCV()) return {SUCCESS, ""};   // all registers restored in template
+    return {E_UNSUPPORTED_ARCH, ""};
 }
 
 template <typename T> std::string genSetRegister(MCRegister Reg, T Value) {
@@ -510,7 +494,7 @@ template <typename T> std::string genSetRegister(MCRegister Reg, T Value) {
     }
     std::string result;
     llvm::raw_string_ostream os(result);
-    for (RegInitTemplate regTemplate : getTemplate(getEnv().Arch).regInitTemplates) {
+    for (RegInitTemplate regTemplate : getTemplate().regInitTemplates) {
         MCRegisterClass movClass = getEnv().MRI->getRegClass(regTemplate.targetRegisterClassID);
         if (!getEnv().regInRegClass(Reg, movClass)) {
             // this can not be used by the template directly, check if the register has any
@@ -540,7 +524,7 @@ template <typename T> std::string genSetRegister(MCRegister Reg, T Value) {
 std::string genRegInitCode(std::vector<MCInst> Instructions, uint64_t RegInitValue) {
     // override some register types
     std::map<unsigned, std::variant<uint32_t, uint64_t, float, double>> regInitMap;
-    if (getEnv().Arch == Triple::ArchType::x86_64) {
+    if (getEnv().isX86()) {
         regInitMap = {
             {X86::VK8WMRegClassID, uint32_t{0b11111111}}, // x86 mask register
             // {X86::GR32RegClassID, float(7.0)},
@@ -597,11 +581,9 @@ ErrorCode isValid(unsigned Opcode) {
     if (desc.isMetaInstruction()) return S_IS_META_INSTRUCTION;
     if (desc.isReturn()) return S_IS_RETURN;
     if (desc.isBranch()) return S_IS_BRANCH; // TODO uops has TP, how?
-    if (!includeX87FP && getEnv().Arch == Triple::ArchType::x86_64 &&
-        desc.hasImplicitDefOfPhysReg(X86::FPSW))
+    if (!includeX87FP && getEnv().isX86() && desc.hasImplicitDefOfPhysReg(X86::FPSW))
         return S_IS_X87FP;
-    if (!includeNonX87FP && getEnv().Arch == Triple::ArchType::x86_64 &&
-        !desc.hasImplicitDefOfPhysReg(X86::FPSW))
+    if (!includeNonX87FP && getEnv().isX86() && !desc.hasImplicitDefOfPhysReg(X86::FPSW))
         return S_IS_NON_X87FP;
     for (auto op : desc.operands())
         if (op.OperandType == MCOI::OPERAND_PCREL) return S_PCREL_OPERAND;
@@ -611,9 +593,9 @@ ErrorCode isValid(unsigned Opcode) {
     // (didn't test which one)
     // in general everything modifying the stack pointer will break
     std::vector<MCRegister> registerBlacklist;
-    if (getEnv().Arch == Triple::ArchType::x86_64)
+    if (getEnv().isX86())
         registerBlacklist = {X86::RSP};
-    else if (getEnv().Arch == Triple::ArchType::aarch64)
+    else if (getEnv().isAArch64())
         registerBlacklist = {AArch64::LR};
 
     ArrayRef<MCPhysReg> defs = desc.implicit_defs();

src/Globals.cpp
@@ -39,7 +39,7 @@ InstructionForm::InstructionForm(unsigned Opcode) : opcode(Opcode), operands({})
     std::vector<unsigned> aarch64MemOffsetIndices;
     std::vector<unsigned> aarch64MemOpIndices;
 
-    if (getEnv().Arch == llvm::Triple::aarch64) {
+    if (getEnv().isAArch64()) {
         unsigned aarch64MemBase = getEnv().getAArch64BaseOperandIndex(Opcode);
         unsigned aarch64MemOffset = getEnv().getAArch64OffsetOperandIndex(Opcode);
         unsigned tiedToMemBase = aarch64MemBase != NO_OP_INDEX
@@ -70,7 +70,7 @@ InstructionForm::InstructionForm(unsigned Opcode) : opcode(Opcode), operands({})
         processedOperands.insert(i);
         auto operandInfo = operandInfos[i];
         // check if this is an Aarch64 memory operand
-        if (getEnv().Arch == llvm::Triple::aarch64 && (contains(aarch64MemOpIndices, i))) {
+        if (getEnv().isAArch64() && (contains(aarch64MemOpIndices, i))) {
             processedOperands.insert(aarch64MemOpIndices.begin(), aarch64MemOpIndices.end());
             operands.emplace_back(
                 OperandForm(currentIndex, aarch64MemOpIndices,
@@ -118,7 +118,7 @@ InstructionForm::InstructionForm(unsigned Opcode) : opcode(Opcode), operands({})
         MCOperandInfo operandInfo = operandInfos[i];
 
         // check if this is an Aarch64 memory operand
-        if (getEnv().Arch == llvm::Triple::aarch64 && (contains(aarch64MemOpIndices, i))) {
+        if (getEnv().isAArch64() && (contains(aarch64MemOpIndices, i))) {
             processedOperands.insert(aarch64MemOpIndices.begin(), aarch64MemOpIndices.end());
             operands.emplace_back(
                 OperandForm(currentIndex, aarch64MemOpIndices,

commit 76f2cb0cf6bf621457bce99bb12b1b1538b40cc5

fix: bug in AArch64 memory operand lookup introduced in 3a3729e

src/LLVMEnvironment.cpp
@@ -274,7 +274,7 @@ unsigned LLVMEnvironment::getTiedToOperand(MCOperandInfo OpInfo) {
 }
 
 unsigned LLVMEnvironment::getAArch64OffsetOperandIndex(unsigned Opcode) {
-    if (getEnv().isAArch64()) return NO_OP_INDEX;
+    if (!getEnv().isAArch64()) return NO_OP_INDEX;
     // verify this is a memory instruction as llvm does in AArch64InstrInfo::verifyInstruction
     TypeSize scale(0U, false), width(0U, false);
     int64_t minOffset, maxOffset;
@@ -287,13 +287,8 @@ unsigned LLVMEnvironment::getAArch64OffsetOperandIndex(unsigned Opcode) {
 }
 
 unsigned LLVMEnvironment::getAArch64BaseOperandIndex(unsigned Opcode) {
-    if (getEnv().isAArch64()) return NO_OP_INDEX;
-    TypeSize scale(0U, false), width(0U, false);
-    int64_t minOffset, maxOffset;
-    if (!AArch64InstrInfo::getMemOpInfo(Opcode, scale, width, minOffset, maxOffset))
-        return NO_OP_INDEX;
-
-    return getAArch64OffsetOperandIndex(Opcode) - 1;
+    unsigned index = getAArch64OffsetOperandIndex(Opcode);
+    return index == NO_OP_INDEX ? NO_OP_INDEX : index - 1;
 }
 
 bool LLVMEnvironment::mayAccessMemory(unsigned Opcode) {