RRZE-HPC/WINIC

commit 24f9dbe2b9cfcd54826c0837905200b9733b28a5

fix do not unroll the memory base register reset added in 965c8c6

src/BenchmarkGenerator.cpp
@@ -133,13 +133,14 @@ genLatBenchmark(const std::vector<LatMeasurement> &Measurements, unsigned *Targe
         }
     }
 
-    std::string loopCode;
     // if this benchmark accesses memory, set the memory base register to the buffer address each
     // iteration so loads/stores with auto-increment don't segfault
+    std::string setMemCode = "";
     for (auto m : Measurements)
         if (getEnv().mayAccessMemory(m.opcode))
-            loopCode = str(benchTemplate.setScratchMemoryBaseReg, "\n");
+            setMemCode = str(benchTemplate.setScratchMemoryBaseReg, "\n");
 
+    std::string loopCode;
     llvm::raw_string_ostream lco(loopCode);
     for (unsigned i = 0; i < *TargetInstrCount; ++i) {
         for (auto inst : instructions) {
@@ -162,10 +163,10 @@ genLatBenchmark(const std::vector<LatMeasurement> &Measurements, unsigned *Targe
 
     AssemblyFile assemblyFile;
     assemblyFile.addInitFunction("init", initCode);
-    assemblyFile.addBenchFunction("lat", saveRegs + regInit, loopCode, restoreRegs, "init",
-                                  *TargetInstrCount * instructions.size());
-    assemblyFile.addBenchFunction("lat2", saveRegs + regInit, loopCode + loopCode, restoreRegs,
-                                  "init", *TargetInstrCount * instructions.size() * 2);
+    assemblyFile.addBenchFunction("lat", saveRegs + regInit, setMemCode + loopCode, restoreRegs,
+                                  "init", *TargetInstrCount * instructions.size());
+    assemblyFile.addBenchFunction("lat2", saveRegs + regInit, setMemCode + loopCode + loopCode,
+                                  restoreRegs, "init", *TargetInstrCount * instructions.size() * 2);
 
     // check if each instruction of the sequence has exactly one dependency to the next one.
     // otherwise return a warning
@@ -238,12 +239,14 @@ genTPBenchmark(unsigned Opcode, unsigned *TargetInstrCount, unsigned UnrollCount
         slo << "\n";
     }
 
-    std::string loopCode;
     // if this benchmark accesses memory, set the memory base register to the buffer address each
     // iteration so loads/stores with auto-increment don't segfault
+    std::string setMemCode = "";
     if (getEnv().mayAccessMemory(Opcode) ||
         (HelperOpcode != MAX_UNSIGNED && getEnv().mayAccessMemory(HelperOpcode)))
-        loopCode = str(benchTemplate.setScratchMemoryBaseReg, "\n");
+        setMemCode = str(benchTemplate.setScratchMemoryBaseReg, "\n");
+
+    std::string loopCode;
     for (unsigned i = 0; i < UnrollCount; i++)
         loopCode.append(singleLoopCode);
 
@@ -251,10 +254,10 @@ genTPBenchmark(unsigned Opcode, unsigned *TargetInstrCount, unsigned UnrollCount
 
     AssemblyFile assemblyFile;
     assemblyFile.addInitFunction("init", initCode);
-    assemblyFile.addBenchFunction("tp", saveRegs + regInit, loopCode, restoreRegs, "init",
-                                  instructions.size());
-    assemblyFile.addBenchFunction("tp2", saveRegs + regInit, loopCode + loopCode, restoreRegs,
-                                  "init", instructions.size() * 2);
+    assemblyFile.addBenchFunction("tp", saveRegs + regInit, setMemCode + loopCode, restoreRegs,
+                                  "init", instructions.size());
+    assemblyFile.addBenchFunction("tp2", saveRegs + regInit, setMemCode + loopCode + loopCode,
+                                  restoreRegs, "init", instructions.size() * 2);
     return {SUCCESS, assemblyFile};
 }
 

commit cb48323ece9ff0360196ebb676c4c9223bbf88dc

fix: use same mem offset if instruction writes base register, fixes segfaults introduced in 24f9dbe

include/LLVMEnvironment.h
@@ -152,6 +152,14 @@ class LLVMEnvironment {
     unsigned getAArch64OffsetOperandIndex(unsigned Opcode);
 
     /**
+     * \brief Checks if this instruction writes to it's base/index register while accessing memory.
+     * This happens e.g. for pre/post incrementing instructions on AArch64.
+     * \param Opcode The opcode of the instruction.
+     * \return True, if the instruction has a memory operand and writes to part of it.
+     */
+    bool hasWriteOnMemRegister(unsigned Opcode);
+
+    /**
      * \brief Returns the index of the memory access base register on AArch64.
      * \param Opcode The opcode of the load/store instruction.
      * \return The index of the base register or NO_OP_INDEX if there is none.

src/BenchmarkGenerator.cpp
@@ -270,7 +270,10 @@ genTPLoop(std::vector<unsigned> Opcodes,
         unsigned opcode = Opcodes[i];
         // this is the first generated instruction, all other instructions will use the
         // same registers as this one if they are only read
-        auto [EC, refInst] = genInst(opcode, ConstraintsVector[i], UsedRegisters, Immediate, 0);
+        unsigned memSize = getEnv().getMemoryOperandWidthUpperBound(opcode);
+        unsigned memOffset = getEnv().hasWriteOnMemRegister(opcode) ? memSize : 0;
+        auto [EC, refInst] =
+            genInst(opcode, ConstraintsVector[i], UsedRegisters, Immediate, memOffset);
         if (EC != SUCCESS) return {EC, instructions};
         instructions.push_back(refInst);
 
@@ -288,10 +291,14 @@ genTPLoop(std::vector<unsigned> Opcodes,
         // mid generation)
         std::list<MCInst> tempInstructions;
         for (unsigned j = 0; j < Opcodes.size(); j++) {
-            unsigned long memSize = getEnv().getMemoryOperandWidthUpperBound(Opcodes[j]);
+            unsigned memSize = getEnv().getMemoryOperandWidthUpperBound(Opcodes[j]);
+            // normally we use a different offset for each instruction to avoid dependencies. If
+            // this instruction increments its base register, we should not do that as the scratch
+            // memory will not be large enough
+            unsigned memOffset = getEnv().hasWriteOnMemRegister(Opcodes[j]) ? memSize : i * memSize;
 
             auto [EC, inst] =
-                genInst(Opcodes[j], ConstraintsVector[j], UsedRegisters, Immediate, i * memSize);
+                genInst(Opcodes[j], ConstraintsVector[j], UsedRegisters, Immediate, memOffset);
             if (EC == E_NO_REGISTERS) return {SUCCESS, instructions}; // shorter loops are ok
             if (EC != SUCCESS) return {EC, {instructions}};
             tempInstructions.push_back(inst);

src/LLVMEnvironment.cpp
@@ -291,6 +291,19 @@ unsigned LLVMEnvironment::getAArch64BaseOperandIndex(unsigned Opcode) {
     return index == NO_OP_INDEX ? NO_OP_INDEX : index - 1;
 }
 
+bool LLVMEnvironment::hasWriteOnMemRegister(unsigned Opcode) {
+    InstructionForm instructionForm = InstructionForm(Opcode);
+    const MCInstrDesc &desc = MCII->get(Opcode);
+    for (OperandForm op : instructionForm.getOperands()) {
+        if (!op.isMemory()) continue;
+
+        for (unsigned index : op.getMCIndices()) {
+            if (index < desc.getNumDefs()) return true;
+        }
+    }
+    return false;
+}
+
 bool LLVMEnvironment::mayAccessMemory(unsigned Opcode) {
     const MCInstrDesc &desc = MCII->get(Opcode);
     return desc.mayLoad() || desc.mayStore();